30a783391a0e3aaefec168d7906dc7879a6efcf2
yebin0523 <gim908516@gmail.com>
2026-05-30 14:03:19 +0900

    드롭박스, 글작성, 프로필 페이지 디자인 및 애니메이션 수정

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -46,6 +46,7 @@ function ProfilePage() {
                     </div>
                     <h2 className="profile-nickname">{user.nickname}</h2>
                     <span className="profile-field">#{user.field}</span>
+                    <div className="profile-divider" />
                     <p className="profile-bio">{user.bio ?? "소개가 없습니다."}</p>
                 </div>
 

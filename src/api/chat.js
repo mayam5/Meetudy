48f9539342cac75ec5222a9f6de99a4c1cfd6400
mayam.5@gmail.com <mayam.five@gmail.com>
2026-05-30 22:55:03 +0900

    modify chat.js

--- src/api/chat.js
+++ src/api/chat.js
@@ -1,5 +1,6 @@
 import { Client } from "@stomp/stompjs";
-import SockJS from "sockjs-client";
+// import SockJS from "sockjs-client";
+import SockJS from "sockjs-client/dist/sockjs";
 
 const BASE_URL = "http://localhost:8080";
 
